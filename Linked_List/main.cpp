16418a1abc1e054317b667dff671f55d91fd1cd9
TTNguyen <nttriet9a4@gmail.com>
2018-11-06 14:26:56 +0700

    Initial Commit

---
+++ Linked_List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Linked_List
+//
+//  Created by TT Nguyen on 11/6/18.
+//  Copyright © 2018 TT Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
